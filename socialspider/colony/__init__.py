088c5a3827bf506fdeef9191ba2c983be1bdddb3
Flavio Garcia <piraz@candango.org>
2015-05-28 01:27:35 -0400

    Colony is an application perse and there is no reason to create a component space inside this application. Moved the application component to the it's root.

---
+++ socialspider/colony/__init__.py
@@ -0,0 +1,26 @@
+from iflux.core import IfluxComponent
+import tornado.web
+import os
+
+from colony.handlers import IndexHandler
+
+
+class ColonyComponent(IfluxComponent):
+
+    def __init__(self, name, application, config={}):
+        IfluxComponent.__init__(self, name, application, config)
+        self.db_backend = None
+        self.colonies = {}
+        self.webs = {}
+
+    def get_handlers(self):
+        handlers = [
+            (r'/', IndexHandler),
+            (r"/colony/static/(.*)", tornado.web.StaticFileHandler,
+             {"path": os.path.join(self.get_component_path(), 'static')}),
+        ]
+        return handlers
+
+    def process_config(self):
+        self.colonies = self.config['colonies']
+        self.webs = self.config['webs']
