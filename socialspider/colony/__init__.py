f6757c101ea03f2502198fe83b847d05f95267e8
Flavio Garcia <piraz@candango.org>
2015-06-12 21:41:36 -0400

    Colony uuid is being genarated considering the current datatime. Generated the first web related to the colony.

--- socialspider/colony/__init__.py
+++ socialspider/colony/__init__.py
@@ -1,8 +1,26 @@
+#!/usr/bin/env python
+#
+# Copyright 2015 Flavio Garcia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+
 from iflux.core import IfluxComponent
 import tornado.web
 import os
 
-from colony.handlers import IndexHandler
+from socialspider.colony.handlers import IndexHandler
 
 
 class ColonyComponent(IfluxComponent):
@@ -22,8 +40,10 @@ class ColonyComponent(IfluxComponent):
         return handlers
 
     def process_config(self):
-        self.colonies = self.config['colonies']
-        self.webs = self.config['webs']
+        if 'colonies' in self.config:
+            self.colonies = self.config['colonies']
+        if 'webs' in self.config:
+            self.webs = self.config['webs']
 
     def install(self):
         import socialspider.colony.models as colony_models
@@ -48,15 +68,30 @@ class ColonyComponent(IfluxComponent):
 
         # Creating colony
         base_colony = colony_models.ColonyBase()
+        created_utc = datetime.datetime.utcnow()
         base_colony.uuid = str(uuid.uuid5(
-            uuid.NAMESPACE_DNS, self.colonies['base']['domain']))
+            uuid.NAMESPACE_URL, '%s/%s' % (
+                self.colonies['base']['domain'],
+                created_utc))).replace('-', '')
         base_colony.name = self.colonies['base']['name']
         base_colony.nick = self.colonies['base']['nick']
         base_colony.domain = self.colonies['base']['domain']
-        base_colony.created_at = datetime.datetime.utcnow()
-        base_colony.updated_at = datetime.datetime.utcnow()
+        base_colony.created_at = created_utc
+        base_colony.updated_at = created_utc
 
         session.add(base_colony)
+
+
+        base_web = colony_models.WebBase()
+        base_web.uuid = base_colony.uuid
+        base_web.name = self.webs['default']['name']
+        base_web.nick = self.webs['default']['nick']
+        base_web.domain = self.colonies['base']['domain']
+        base_web.colony_uuid = base_colony.uuid
+        base_web.created_at = created_utc
+        base_web.updated_at = created_utc
+
+        session.add(base_web)
 
         session.commit()
 
@@ -67,4 +102,3 @@ class ColonyComponent(IfluxComponent):
         return self.application.get_connection_handler(
             self.config['connection_instance']
         )
-        self.webs = self.config['webs']
