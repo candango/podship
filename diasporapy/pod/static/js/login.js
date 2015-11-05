eeab970630784a2ed255be11dd5d25aa664aa856
Flavio Garcia <piraz@candango.org>
2015-11-04 20:35:51 -0500

    Mapped the account login handler with the right login form template.
    Still need to finish the complete form validation.
    
    Refs: #7

--- diasporapy/pod/static/js/login.js
+++ diasporapy/pod/static/js/login.js
@@ -1,10 +1,10 @@
 AdminLoginModel = can.Model({
-    create : "POST /admin/login"
+    create : "POST /account/login"
 },{});
 
 var AdminLoginApp = can.Control.extend({
     defaults: {
-        view:'/static/js/views/login_form.ejs'
+        view:'/static/js/views/login_form.stache'
     }
 },{
    init: function() {
@@ -29,14 +29,17 @@ var AdminLoginApp = can.Control.extend({
             },
             function(response) {
                 var errorMessage = '';
-                if(!response.responseJSON.has_user_name) {
+                if(response.responseJSON.errors.hasOwnProperty('username')) {
                     $('#userNameFormGroup').addClass('has-error');
                 }
-                if(!response.responseJSON.has_user_password) {
+                if(response.responseJSON.errors.hasOwnProperty('password')) {
                     $('#passwordFormGroup').addClass('has-error');
                 }
-                response.responseJSON.sources.forEach(
+                var errors = new can.Map(response.responseJSON.errors);
+
+                errors.each(
                     function(element, index, list) {
+                        console.debug(element, index, list);
                         var source = new can.Map(list[index]);
                         var keys = can.Map.keys(source);
                         errorMessage += source.attr(keys[0]) + '<br>';
