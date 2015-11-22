164192df6f23675ed3c9f24fe62a356179026661
Flavio Garcia <piraz@candango.org>
2015-11-21 23:50:34 -0500

    Loaded dependencies using stealjs.
    This commit is breaking the bootstrap and internacionalization
    is not working properly.
    
    Refs: #29

---
+++ diasporapy/pod/static/js/stealconfig.js
@@ -0,0 +1,9 @@
+System.config({
+    paths: {
+        can: "/assets/canjs/amd/can.js",
+        'can/*': "/assets/canjs/amd/can/*.js",
+        bootstrap: "/assets/bootstrap/js/bootstrap.js",
+        i18next: "/assets/i18next/i18next.amd.withJQuery.js",
+        jquery: "/assets/jquery/jquery.js"
+    }
+});
