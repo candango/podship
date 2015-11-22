391408de16b44c4fd7156e093fd437d8204008a9
Flavio Garcia <piraz@candango.org>
2015-11-22 00:54:22 -0500

    Changed can and jquery paths so it be loaded without the './'.
    Kept jquery original path because i18next is using it.
    
    Refs: #29

--- diasporapy/pod/static/js/stealconfig.js
+++ diasporapy/pod/static/js/stealconfig.js
@@ -1,9 +1,9 @@
 System.config({
     paths: {
-        can: "/assets/canjs/amd/can.js",
-        'can/*': "/assets/canjs/amd/can/*.js",
+        "can/*": "/assets/canjs/steal/can/*.js",
         bootstrap: "/assets/bootstrap/js/bootstrap.js",
         i18next: "/assets/i18next/i18next.amd.withJQuery.js",
-        jquery: "/assets/jquery/jquery.js"
+        jquery: "/assets/jquery/jquery.js",
+        "jquery/*": "/assets/jquery/*.js"
     }
 });
