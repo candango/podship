97370548bcb004f4a4465f3adad0fe221281b0aa
Flavio Garcia <piraz@candango.org>
2015-11-27 00:26:10 -0500

    Introduced css paths the stealconfig file.
    Refs: #29

--- diasporapy/pod/static/js/stealconfig.js
+++ diasporapy/pod/static/js/stealconfig.js
@@ -1,5 +1,7 @@
 System.config({
     paths: {
+        "assets/bootstrap/css/*.css": "/assets/bootstrap/css/*.css",
+        "static/css/*.css": "/static/css/*.css",
         "can/*": "/assets/canjs/steal/can/*.js",
         bootstrap: "/assets/bootstrap/js/bootstrap.js",
         i18next: "/assets/i18next/i18next.amd.withJQuery.js",
