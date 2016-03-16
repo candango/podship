0c4c50a5c7b4b9e1a92871c72a5dae8565946224
Flavio Garcia <piraz@candango.org>
2016-03-15 23:07:18 -0400

    Moved the RabbiMQ code to RabbitMQClient to clean the app component.
    Refs: #40.

--- diasporapy/engine/application.py
+++ diasporapy/engine/application.py
@@ -31,35 +31,84 @@ logger = logging.getLogger(__name__)
 # Based on https://gist.github.com/brimcfadden/2855520
 class RabbitMQClient(object):
 
-    def __init__(self):
+    def __init__(self, conf):
+        self.channels = {}
+        self.channels['in'] = None
+        self.channels['out'] = {}
+        self.conf = conf
         self.connecting = False
         self.connection = None
-        self.channel = None
-        self.in_channel = None
-        self.out_channels = {}
 
     def connect(self):
-        if self.connecting:
-            pika.log.info('Already connecting to RabbitMQ')
-            return
-        pika.log.info("Connecting to RabbitMQ")
+        logger.info("Connecting to RabbitMQ")
+
         self.connecting = True
+
+        creds = pika.PlainCredentials(
+            self.conf['user'],
+            self.conf['pass']
+        )
+        params = pika.ConnectionParameters(
+            host=self.conf['host'],
+            port=self.conf['port'],
+            virtual_host='/',
+            credentials=creds
+        )
+
+        TornadoConnection(
+            params,
+            on_open_callback=self.on_connection_opened,
+            on_close_callback=self.on_connection_closed,
+            on_open_error_callback=self.on_connection_failed
+        )
+
+    def disconnect(self):
+        logger.info('Disconnecting Engine from RabbitMQ')
+        self.connection.close()
+
+    def on_connection_opened(self, connection):
+        logger.info('Engine connected to RabbitMQ')
+        self.connection = connection
+        self.connection.channel(self.on_channel_opened)
+
+    def on_channel_opened(self, channel):
+        logger.info('Engine channel opened with RabbitMQ')
+        self.channels['in'] = channel
+        self.channels['in'].exchange_declare(
+            exchange=EXCHANGE_NAME,
+            type='topic')
+        self.channels['in'].queue_declare(
+            callback=self.on_input_queue_declared,
+            queue=INPUT_QUEUE_NAME)
+        logger.info('Engine component initialized')
+
+    def on_input_queue_declared(self, queue):
+        logger.info('Engine input queue declared on RabbitMQ')
+        self.channels['in'].queue_bind(callback=None,
+                                       exchange=EXCHANGE_NAME,
+                                       queue=INPUT_QUEUE_NAME,
+                                       routing_key="#")
+
+    def on_connection_closed(self, connection, code, message):
+        logger.info('Engine disconnected from RabbitMQ')
+
+    def on_connection_failed(self, connection, error_message):
+        logger.error('Engine was no able to connect to RabbitMQ, '
+                     'terminating the node. Cause: %s' % error_message)
+        import tornado.ioloop
+        tornado.ioloop.IOLoop.instance().stop()
 
 EXCHANGE_NAME = 'gateway_in'
 
 INPUT_QUEUE_NAME = 'gateway_in_queue'
+
 
 class EngineComponent(firenado.core.TornadoComponent):
 
     def __init__(self, name, application):
         super(EngineComponent, self).__init__(name, application)
         self.rabbitmq = {}
-        self.rabbitmq['channels'] = {}
-        self.rabbitmq['channels']['in'] = None
-        self.rabbitmq['channels']['out'] = {}
-        self.rabbitmq['conf'] = None
-        self.rabbitmq['connecting'] = False
-        self.rabbitmq['connection'] = None
+        self.rabbitmq['client'] = None
         self.project_root = os.path.abspath(
                 os.path.join(os.path.dirname(__file__), '..'))
 
@@ -75,66 +124,14 @@ class EngineComponent(firenado.core.TornadoComponent):
         return 'engine'
 
     def initialize(self):
-        self.rabbitmq['conf'] = load_yaml_config_file(
-            os.path.join(self.project_root, 'conf', 'rabbitmq.yml'))
-        logger.info('Initializing engine component')
-
-        logger.info("Connecting to RabbitMQ")
-
-        self.rabbitmq['connecting'] = True
-
-        creds = pika.PlainCredentials(
-            self.rabbitmq['conf']['user'],
-            self.rabbitmq['conf']['pass']
-        )
-        params = pika.ConnectionParameters(
-            host=self.rabbitmq['conf']['host'],
-            port=self.rabbitmq['conf']['port'],
-            virtual_host='/',
-            credentials=creds
-        )
-
-        TornadoConnection(
-            params,
-            on_open_callback=self.rabbitmq_connection_opened,
-            on_close_callback=self.rabbitmq_connection_closed,
-            on_open_error_callback=self.rabbitmq_connection_failed
-        )
+        self.rabbitmq['client'] = RabbitMQClient(
+            load_yaml_config_file(os.path.join(
+                self.project_root, 'conf', 'rabbitmq.yml')))
+        self.rabbitmq['client'].connect()
 
     def shutdown(self):
-        self.rabbitmq['connection'].close()
+        self.rabbitmq['client'].disconnect()
 
-    def rabbitmq_connection_opened(self, connection):
-        logger.info('Engine connected to RabbitMQ')
-        self.rabbitmq['connection'] = connection
-        self.rabbitmq['connection'].channel(self.rabbitmq_channel_opened)
-
-    def rabbitmq_channel_opened(self, channel):
-        logger.info('Engine channel opened with RabbitMQ')
-        self.rabbitmq['channels']['in'] = channel
-        self.rabbitmq['channels']['in'].exchange_declare(
-            exchange=EXCHANGE_NAME,
-            type='topic')
-        self.rabbitmq['channels']['in'].queue_declare(
-            callback=self.rabbitmq_input_queue_declared,
-            queue=INPUT_QUEUE_NAME)
-        logger.info('Engine component initialized')
-
-    def rabbitmq_input_queue_declared(self, queue):
-        logger.info('Engine input queue declared on RabbitMQ')
-        self.rabbitmq['channels']['in'].queue_bind(callback=None,
-                                                   exchange=EXCHANGE_NAME,
-                                                   queue=INPUT_QUEUE_NAME,
-                                                   routing_key="#")
-
-    def rabbitmq_connection_closed(self, connection, code, message):
-        logger.info('Engine disconnected from RabbitMQ')
-
-    def rabbitmq_connection_failed(self, connection, error_message):
-        logger.error('Engine was no able to connect to RabbitMQ, '
-                     'terminating the node. Cause: %s' % error_message)
-        import tornado.ioloop
-        tornado.ioloop.IOLoop.instance().stop()
 
 if __name__ == '__main__':
     import firenado.conf
